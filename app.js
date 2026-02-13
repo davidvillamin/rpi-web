c9858bdd28bea7ea03bdab0a9e3d610ef12c32c1
davidvillamin <davidjohnvillamin@gmail.com>
2026-02-13 11:17:55 +0800

    add sitemap

--- app.js
+++ app.js
@@ -12,6 +12,21 @@ app.get("/", function(req, res) {
 	res.render("maintenance/index");
 });
 
+app.get('/sitemap.xml', function(req, res) {
+    res.header('Content-Type', 'application/xml');
+    res.send(`<?xml version="1.0" encoding="UTF-8"?>
+        <urlset
+            xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                    http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+            <url>
+                <loc>https://djvitsolutions.com/</loc>
+                <lastmod>2026-02-13T02:50:12+00:00</lastmod>
+            </url>
+        </urlset>`);
+});
+
 app.use(function(req, res) {
     res.status(404).render("error", {
         title: "Page not found",
